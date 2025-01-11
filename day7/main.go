156c02f385dcd0735dab93e6b94286aa27eb7665
Eugene Potapov <epotapov@zello.com>
2025-01-11 15:59:10 -0600

    day7 v2

--- day7/main.go
+++ day7/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -13,7 +14,12 @@ type test struct {
 	arguments []int
 }
 
-func (t test) verify() bool {
+func concatenate(a int, b int) int {
+	result, _ := strconv.Atoi(fmt.Sprintf("%d%d", a, b))
+	return result
+}
+
+func (t test) verify1() bool {
 	if len(t.arguments) == 1 {
 		return t.expected == t.arguments[0]	
 	}
@@ -30,6 +36,41 @@ func (t test) verify() bool {
 		}
 		if result == t.expected {
 			return true
+		}
+		if result > t.expected {
+			continue
+		}
+	}
+	return false
+}
+
+
+func (t test) verify2() bool {
+	if len(t.arguments) == 1 {
+		return t.expected == t.arguments[0]	
+	}
+	opCount := len(t.arguments) - 1
+	optionsTotal := int(math.Pow(3, float64(opCount)))
+	for i := 0; i < optionsTotal; i++ {
+		operations := strconv.FormatInt(int64(i), 3)
+		if zerosNeeded := opCount - len(operations); zerosNeeded > 0 {
+			operations = strings.Repeat("0", zerosNeeded) + operations
+		}
+		result := t.arguments[0]
+		for op := 0; op < opCount; op++ {
+			if operations[op] == '0'{
+				result += t.arguments[op + 1]
+			} else if operations[op] == '1' {
+				result *= t.arguments[op + 1]
+ 			} else {
+				result = concatenate(result, t.arguments[op + 1])
+			}
+		}
+		if result == t.expected {
+			return true
+		}
+		if result > t.expected {
+			continue
 		}
 	}
 	return false
@@ -49,23 +90,29 @@ func newTest(s string) test {
 	return t
 }
 
-func checkEquastion(s string) int {
+func checkEquastion(s string) (int, int) {
 	t := newTest(s)
-	if t.verify() {
-		return t.expected
+	result1, result2 := 0, 0
+	if t.verify2() {
+		result2 = t.expected
 	}
-	return 0
+	if t.verify1() {
+		result1 = t.expected
+	}
+	return result1, result2
 }
 
 func calc(filename string) (int, int) {
 	file, _ := os.Open(filename)
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
-	total := 0
+	total1, total2 := 0, 0
 	for scanner.Scan() {
-		total += checkEquastion(scanner.Text())
+		r1, r2 := checkEquastion(scanner.Text())
+		total1 += r1
+		total2 += r2
 	}
-	return total, 0
+	return total1, total2
 }
 
 func runForFile(filename string) {
