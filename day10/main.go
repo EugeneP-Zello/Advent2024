d92a1483724a1514b07275c67db27c3ebdb7430b
Eugene Potapov <epotapov@zello.com>
2025-01-12 13:20:54 -0600

    day10 v2

--- day10/main.go
+++ day10/main.go
@@ -14,21 +14,23 @@ var directions = [][2]int{
 	{0, 1},
 }
 	
-func nextStep(x, y, elevation int, world [][]int, finishes map[[2]int]bool) {
+func nextStep(x, y, elevation int, world [][]int, finishes map[[2]int]bool) int {
 	if x < 0 || y < 0 || x >= len(world[0]) || y >= len(world) {
-		return;
+		return 0;
 	}
 	if world[x][y] != elevation {
-		return;
+		return 0;
 	}
 	if elevation == 9 {
 		finishes[[2]int{x, y}] = true
-		return
+		return 1
 	}
+	trails := 0
 	for _, direction := range directions {
 		nextX, nextY := x + direction[0], y + direction[1]
-		nextStep(nextX, nextY, elevation + 1, world, finishes)
+		trails += nextStep(nextX, nextY, elevation + 1, world, finishes)
 	}
+	return trails
 }
 
 func calc(filename string) (int, int) {
@@ -46,24 +48,25 @@ func calc(filename string) (int, int) {
 	}
 
 	count := 0
+	rating := 0 
 	// now iterate through the world, set obstacles and count loops
 	for i, row := range worldMap {
 		for j, elevation := range row {
 			if elevation == 0 {
 				finishes := make(map[[2]int]bool)
-				nextStep(i, j, 0, worldMap, finishes)
+				rating += nextStep(i, j, 0, worldMap, finishes)
 				for range finishes {
 					count++
 				}
 			}
 		}
 	}
-	return count, 0
+	return count, rating
 }
 
 func runForFile(filename string) {
-	ssc, lc := calc(filename)
-	fmt.Printf("File: %s, SumScore: %d, 2nd: %d\n", filename, ssc, lc)
+	ssc, rating := calc(filename)
+	fmt.Printf("File: %s, SumScore: %d, Rating: %d\n", filename, ssc, rating)
 }
 
 func main() {
