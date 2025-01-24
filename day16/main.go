6fc2daeeae7585e04091b38f5a646da570d313a0
Eugene Potapov <epotapov@zello.com>
2025-01-24 00:53:55 -0600

    day16

--- day16/main.go
+++ day16/main.go
@@ -23,6 +23,7 @@ var directions = []Point{
 type Coords struct {
 	x, y, dir int
 	Score int
+	Path []*Coords
 }
 
 type PriorityQueue []*Coords
@@ -49,52 +50,52 @@ func (pq *PriorityQueue) GetMinValue() *Coords {
 	return heap.Pop(pq).(*Coords)
 }
 
-func move(c Coords) Coords {
-	return Coords{
+func move(c *Coords) *Coords {
+	Path := make([]*Coords,len(c.Path))
+	copy(Path, c.Path)
+	return &Coords{
 		x: c.x + directions[c.dir].x,
 		y: c.y + directions[c.dir].y,
 		dir: c.dir,
-		Score: c.Score +1,
+		Score: c.Score + 1,
+		Path: append(Path, c),
 	}
 }
 
-func left(c Coords) Coords {
+func left(c *Coords) *Coords {
 	newDir := c.dir - 1
 	if newDir < 0 {
 		newDir = 3
 	}
-	return Coords{
+	return &Coords{
 		x: c.x,
 		y: c.y,
 		dir: newDir,
 		Score: c.Score + 1000,
+		Path: c.Path,
 	}
 }
 
-func right(c Coords) Coords {
+func right(c *Coords) *Coords {
 	newDir := c.dir + 1
 	if newDir > 3 {
 		newDir = 0
 	}
-	return Coords{
+	return &Coords{
 		x: c.x,
 		y: c.y,
 		dir: newDir,
 		Score: c.Score + 1000,
+		Path: c.Path,
 	}
 }
 
-func PosIndex(c Coords) int {
+func PosIndex(c *Coords) int {
 	return c.x + 1000*c.y + 1000000*c.dir
 }
 
-type Position struct {
-	Score	int
-	Coords
-}
-
-func Next(current Coords, world [][]rune, visited map[int]bool) []Coords {
-	r := make([]Coords, 0)
+func Next(current *Coords, world [][]rune) []*Coords {
+	r := make([]*Coords, 0)
 	p1 := move(current)
 	if world[p1.y][p1.x] != '#' {
 		r = append(r, p1)
@@ -118,9 +119,10 @@ func calcScore(filename string) (int, int) {
 	file.Close()
 	rows := strings.Split(string(content), "\r\n")
 	start := Coords {
-	  dir: 1,	
+	  dir: 1,
+	  Path: make([]*Coords, 0),	
 	}
-	finish := Coords{}
+	finish := Coords{Path: make([]*Coords, 0),}
 	worldMap := make([][]rune, len(rows))
 	for y, row := range rows {
 		worldMap[y] = make([]rune, len(row))
@@ -145,52 +147,57 @@ func calcScore(filename string) (int, int) {
 	pq := make(PriorityQueue, 1)
 	pq[0] = &start
 	heap.Init(&pq)
-	visited := make(map[int]bool)
-	scores := make(map[int]int)
-	scores[start.y*1000+start.x] = 0
+	bestPositions := make(map[int]bool)
+	visited := make(map[int]int)
 	next := make([]Coords, 1)
 	next[0] = start
-	for len(next) > 0 {
-		second := make([]Coords, 0)
-		for _, crd := range next {
-			idx := PosIndex(crd)
-			if !visited[idx] {
-				// fmt.Printf("{%2d,%2d}\r\n", crd.y, crd.x)
-				visited[idx] = true
-				for _, newCrd := range Next(crd, worldMap, visited) {
-					idxScore := newCrd.y *1000 + newCrd.x
-					if value, exists := scores[idxScore]; !exists || value > newCrd.Score {
-						scores[idxScore] = newCrd.Score
-						// fmt.Printf("{%2d,%2d} - %d\r\n", crd.y, crd.x, newCrd.Score)
-					}
-					second = append(second, newCrd)
-				}
-			}
+	score := 0
+	for pq.Len() > 0 {
+		crd := pq.GetMinValue()
+		if score > 0 && score < crd.Score {
+			continue
 		}
-		next = second
+		idx := PosIndex(crd)
+		if prevScore, exists := visited[idx]; exists && prevScore < crd.Score {
+			continue
+		}
+		if crd.x == finish.x && crd.y == finish.y {
+			if score > 0 && score < crd.Score {
+				break
+			}
+			score = crd.Score
+			bestPositions[crd.y *1000 + crd.x] = true
+			for _, v := range crd.Path {
+				idxScore := v.y *1000 + v.x
+				bestPositions[idxScore] = true
+				worldMap[v.y][v.x] = 'O'
+			}
+			continue
+		}
+		visited[idx] = crd.Score
+		for _, newCrd := range Next(crd, worldMap) {
+			pq.Push(newCrd)
+		}
+	}
+
+	fmt.Println("\r\n")
+	for _, row := range worldMap {
+		fmt.Println(string(row))
 	}
 	
-	lowestScore := scores[finish.y*1000+finish.x]
-
-	return lowestScore, 0
+	count := len(bestPositions)
+	return score, count 
 }
 
 func runForFile(filename string) {
-	Score, Score2 := calcScore(filename)
-	fmt.Printf("File: %s, Score#1 : %d, score#2: %d\n", filename, Score, Score2)
+	Score, BestTilesCount := calcScore(filename)
+	fmt.Printf("File: %s, Score : %d, best tiles count: %d\n", filename, Score, BestTilesCount)
 }
 
 
 func main() {
-//	runForFile("test3.txt")
-//	runForFile("test.txt")
-//	runForFile("test2.txt")
-//	runForFile("input.txt")
-	file, _ := os.Open("input.txt")
-	content, _ := io.ReadAll(file)
-	file.Close()
-	rows := strings.Split(string(content), "\r\n")
-	rows[0] = ""
-	fmt.Printf("\r\n")
-	day16_1(string(content))
+	runForFile("test3.txt")
+	runForFile("test.txt")
+	runForFile("test2.txt")
+	runForFile("input.txt")
 }
